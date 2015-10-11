6469fcb1948386123db2bdbbe871d7051a1db78b
rmtrigo <rmontrigo@gmail.com>
2015-10-10 22:17:01 -0700

    Earned Design a better app badge

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,13 +2,8 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -17,17 +12,25 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        // Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        Group root = new Group();
-        Text txt = new Text("Sup?");
-        TextField nameFld = new TextField();
-        Button btn = new Button();
-        btn.setText("Say Sup!");
-        btn.setOnAction(evt -> System.out.printf("Sup %s!%n", nameFld.getText()));
-        txt.setY(50);
-        VBox box = new VBox();
-        box.getChildren().addAll(txt, nameFld, btn);
-        root.getChildren().add(box);
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
+//        Group root = new Group();
+//        Text txt = new Text("Sup?");
+//        txt.setFont(new Font("Papyrus", 80));
+//        Label label = new Label("Name:");
+//        TextField nameFld = new TextField();
+//        GridPane grid = new GridPane();
+//        grid.add(label, 0, 0);
+//        grid.add(nameFld, 1, 0);
+//        grid.setHgap(20);
+//        Button btn = new Button();
+//        grid.add(btn, 1, 1);
+//        // grid.setGridLinesVisible(true);
+//        btn.setText("Say Sup!");
+//        btn.setOnAction(evt -> System.out.printf("Sup %s!%n", nameFld.getText()));
+//        txt.setY(50);
+//        VBox box = new VBox();
+//        box.getChildren().addAll(txt, grid);
+//        root.getChildren().add(box);
         primaryStage.setTitle("Sup");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
